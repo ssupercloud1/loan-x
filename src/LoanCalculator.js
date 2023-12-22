4311c1ef5d328e1b71490bbde9d7409f772cc89a
Bonface Juma <bonface.juma15@gmail.com>
2023-12-22 18:53:00 +0300

    modified LoanCalculator.js file

--- src/LoanCalculator.js
+++ src/LoanCalculator.js
@@ -1,3 +1,5 @@
+// LoanCalculator.js
+
 import React, { useState } from 'react';
 
 const LoanCalculator = () => {
@@ -5,11 +7,11 @@ const LoanCalculator = () => {
   const [interestRate, setInterestRate] = useState('');
   const [loanTerm, setLoanTerm] = useState('');
   const [monthlyPayment, setMonthlyPayment] = useState(null);
+  const [processingFees, setProcessingFees] = useState(null);
+  const [insuranceFees, setInsuranceFees] = useState(null);
+  const [ledgerFees, setLedgerFees] = useState(null);
 
   const handleCalculate = () => {
-    // Perform loan calculation logic here
-    // You may want to use a library like financial.js for precise calculations
-    // For simplicity, we'll use a basic formula here
     const principal = parseFloat(loanAmount);
     const rate = parseFloat(interestRate) / 100 / 12;
     const periods = parseFloat(loanTerm) * 12;
@@ -17,11 +19,33 @@ const LoanCalculator = () => {
     const monthlyPayment =
       (principal * rate) / (1 - Math.pow(1 + rate, -periods));
 
+    const processingFees = principal * 0.025; // 2.5% of total loan
+    const insuranceFees = principal * 0.015; // 1.5% of total loan
+    const ledgerFees = principal * 0.01; // 1.0% of outstanding loan
+
     setMonthlyPayment(monthlyPayment.toFixed(2));
+    setProcessingFees(processingFees.toFixed(2));
+    setInsuranceFees(insuranceFees.toFixed(2));
+    setLedgerFees(ledgerFees.toFixed(2));
+  };
+
+  const handleReset = () => {
+    setLoanAmount('');
+    setInterestRate('');
+    setLoanTerm('');
+    setMonthlyPayment(null);
+    setProcessingFees(null);
+    setInsuranceFees(null);
+    setLedgerFees(null);
   };
 
   return (
     <div>
+      <div>
+        <a href="https://www.financingfix.com" target="_blank" rel="noopener noreferrer">
+          Visit Our Website
+        </a>
+      </div>
       <label>
         Loan Amount (KES):
         <input
@@ -53,11 +77,15 @@ const LoanCalculator = () => {
       <br />
 
       <button onClick={handleCalculate}>Calculate</button>
+      <button onClick={handleReset}>Reset</button>
 
       {monthlyPayment !== null && (
         <div>
-          <h2>Monthly Payment:</h2>
-          <p>{monthlyPayment} KES</p>
+          <h2>Loan Summary:</h2>
+          <p>Monthly Payment: {monthlyPayment} KES</p>
+          <p>Processing Fees: {processingFees} KES</p>
+          <p>Insurance Fees: {insuranceFees} KES</p>
+          <p>Annual Ledger Fees: {ledgerFees} KES</p>
         </div>
       )}
     </div>
